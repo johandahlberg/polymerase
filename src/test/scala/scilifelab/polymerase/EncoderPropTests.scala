25132261cf0ab6e36e79efb94e2c3eda51f7cd7e
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2019-07-05 09:19:48 +0200

    Clean-up

--- src/test/scala/scilifelab/polymerase/EncoderPropTests.scala
+++ src/test/scala/scilifelab/polymerase/EncoderPropTests.scala
@@ -6,8 +6,6 @@ import org.scalacheck.Prop.forAll
 object EncoderDecoderSpecification
     extends Properties("Polymerase Encoders/Decoders") {
 
-  //TODO Write some properties that check the encoding decoding of raw bytes..
-
   property("Encoding and decoding a Byte should return original Byte") =
     forAll { a: Byte =>
       a == DNACodec.decode(DNACodec.encode(a).toIterator).next()
@@ -28,11 +26,4 @@ object EncoderDecoderSpecification
     a: Byte =>
       DNACodec.encode(a).toList.length == 4
   }
-//  property("encoding and then decoding should return the same data") = forAll {
-//    a: String =>
-//      println(f"Empty String? ${a.isEmpty()}")
-//      a.getBytes == DNACodec.decode(DNACodec.encode(a.getBytes))
-//  }
-
-  // Encoded length should be smaller than original byte length...
 }
