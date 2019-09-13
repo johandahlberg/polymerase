1330f155be19d9c4b0340233cf2bacaffbd71b2b
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2019-09-13 14:53:50 +0200

    Test when there is an error in DNA

--- src/test/scala/se/scilifelab/polymerase/RSEncoderPropTests.scala
+++ src/test/scala/se/scilifelab/polymerase/RSEncoderPropTests.scala
@@ -11,12 +11,24 @@ object RSEncoderDecoderSpecification
   val nonEmptyBytes = nonEmptyListOf(arbitrary[Byte])
 
   property(
-    "Encoding and decoding a stream of bytes should return the original byte"
+    "Encoding and decoding a stream of bytes should return " +
+      "the original byte when there are no errors"
   ) = forAll(nonEmptyBytes) { a =>
     val res =
       ReedSolomonDNACodec
         .decode(ReedSolomonDNACodec.encode(a.toIterator))
         .toList
+    a == res
+  }
+
+  property(
+    "Encoding and decoding a stream of bytes should return " +
+      "the original byte when there are are errors"
+  ) = forAll(nonEmptyBytes) { a =>
+    val encoded = ReedSolomonDNACodec.encode(a.toIterator).toList
+    val garbled = encoded.updated(encoded.indexOf('A'), 'T')
+    require(encoded != garbled)
+    val res = ReedSolomonDNACodec.decode(garbled.toIterator).toList
     a == res
   }
 
