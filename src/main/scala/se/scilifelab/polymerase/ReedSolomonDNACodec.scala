cdc72d15d6f689889c0262438f84490eb18eecee
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2019-09-17 07:02:11 +0200

    Shorter message size

--- src/main/scala/se/scilifelab/polymerase/ReedSolomonDNACodec.scala
+++ src/main/scala/se/scilifelab/polymerase/ReedSolomonDNACodec.scala
@@ -5,10 +5,13 @@ import se.scilifelab.reedsolomon.{Defaults => RSDefaults}
 
 object ReedSolomonDNACodec {
 
+  lazy val messageLength = 80
+  lazy val codeWordLength = 90
+
   // TODO These settings generate to long DNA sequences
   // per block. Look at better values.
   private val rsCoder =
-    ReedSolomonCoder(n = RSDefaults.dictonarySize, k = RSDefaults.messageSize)
+    ReedSolomonCoder(n = messageLength, k = codeWordLength)
 
   // TODO Psudo randomize bytes, to avoid monomer stretches.
   //      Or better still, rotate bases as described in X.
@@ -16,7 +19,7 @@ object ReedSolomonDNACodec {
   def encode(data: Iterator[Byte]): Iterator[Nucleotide] = {
 
     val groupedDataWithLength = data
-      .grouped(RSDefaults.messageSize - Integer.BYTES)
+      .grouped(messageLength - Integer.BYTES)
       .map(x => (x.length +: x.map(y => y & (0xff))))
 
     val rsEncodedData = groupedDataWithLength.map { mess =>
@@ -40,7 +43,7 @@ object ReedSolomonDNACodec {
         // There is 8 bits per encoded byte, and 2 bits encoded
         // per nucleotide.
         // 8 / 2 = 4 which is the number modifer for the message size.
-        .grouped(RSDefaults.messageSize * 4)
+        .grouped(messageLength * 4)
 
     byteBlocks
       .map { x =>
