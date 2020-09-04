d0c00eee9fd923073c9289be8b3777fe9554670e
Johan Dahlberg <johan@uppsala-bioinformatics.se>
2020-09-04 13:25:06 +0200

    Add basic tests back

--- src/test/scala/se/scilifelab/polymerase/fountaincodes/FountainCodesSpec.scala
+++ src/test/scala/se/scilifelab/polymerase/fountaincodes/FountainCodesSpec.scala
@@ -38,65 +38,69 @@ class FountainCodeSpec extends AnyFlatSpec {
 
   }
 
-  //it should "encode and decode input data (2)" in {
+  it should "encode and decode input data (2)" in {
 
-  //  val inputData: Seq[Seq[Byte]] =
-  //    Seq(
-  //      Seq(1, 2, 3, 4, 5),
-  //      Seq(6, 7, 8, 9, 10)
-  //    )
-  //  val codec = new FountainsCodes()
-  //  val encoded = codec.encode(inputData)
-  //  val (decoded, nbrOfBlocksSolved) =
-  //    codec.decode(encoded.toSeq, inputData.length)
+    val inputData = dataToUnencodedPackages(
+      Seq(
+        Seq(1, 2, 3, 4, 5),
+        Seq(6, 7, 8, 9, 10)
+      )
+    )
+    val codec = new FountainsCodes()
+    val encoded = codec.encode(inputData)
+    val (decoded, nbrOfBlocksSolved) =
+      codec.decode(encoded.toSeq, inputData.length)
 
-  //  nbrOfBlocksSolved should be(inputData.length)
-  //  decoded should be(inputData)
-  //}
+    nbrOfBlocksSolved should be(inputData.length)
+    decoded.map(_.data.toSeq) should be(inputData.map(_.data.toSeq))
+  }
 
-  //it should "encode and decode input data (3)" in {
+  it should "encode and decode input data (3)" in {
 
-  //  val inputData: Seq[Seq[Byte]] =
-  //    Seq(
-  //      Seq(1, 2, 3, 4, 5),
-  //      Seq(6, 7, 8, 9, 10),
-  //      Seq(11, 12, 13, 14, 15),
-  //      Seq(16, 17, 18, 19, 20)
-  //    )
-  //  val codec = new FountainsCodes()
-  //  val encoded = codec.encode(inputData)
-  //  val (decoded, nbrOfBlocksSolved) =
-  //    codec.decode(encoded.toSeq, inputData.length)
+    val inputData = dataToUnencodedPackages(
+      Seq(
+        Seq(1, 2, 3, 4, 5),
+        Seq(6, 7, 8, 9, 10),
+        Seq(11, 12, 13, 14, 15),
+        Seq(16, 17, 18, 19, 20)
+      )
+    )
+    val codec = new FountainsCodes()
+    val encoded = codec.encode(inputData)
+    val (decoded, nbrOfBlocksSolved) =
+      codec.decode(encoded.toSeq, inputData.length)
 
-  //  nbrOfBlocksSolved should be(inputData.length)
-  //  decoded should be(inputData)
-  //}
+    nbrOfBlocksSolved should be(inputData.length)
+    decoded.map(_.data.toSeq) should be(inputData.map(_.data.toSeq))
+  }
 
-  //it should "encode and decode input data (4)" in {
+  it should "encode and decode input data (4)" in {
 
-  //  val inputData: Seq[Seq[Byte]] =
-  //    Seq(
-  //      Seq(1, 2, 3, 4, 5)
-  //    )
-  //  val codec = new FountainsCodes()
-  //  val encoded = codec.encode(inputData)
-  //  val (decoded, nbrOfBlocksSolved) =
-  //    codec.decode(encoded.toSeq, inputData.length)
+    val inputData = dataToUnencodedPackages(
+      Seq(
+        Seq(1, 2, 3, 4, 5)
+      )
+    )
+    val codec = new FountainsCodes()
+    val encoded = codec.encode(inputData)
+    val (decoded, nbrOfBlocksSolved) =
+      codec.decode(encoded.toSeq, inputData.length)
 
-  //  nbrOfBlocksSolved should be(inputData.length)
-  //  decoded should be(inputData)
-  //}
+    nbrOfBlocksSolved should be(inputData.length)
+    decoded.map(_.data.toSeq) should be(inputData.map(_.data.toSeq))
+  }
 
-  //it should "encode and decode input data (5)" in {
+  it should "encode and decode input data (5)" in {
 
-  //  val inputData: Seq[Seq[Byte]] =
-  //    Seq.fill(5)(Seq.fill(5)(0))
-  //  val codec = new FountainsCodes()
-  //  val encoded = codec.encode(inputData)
-  //  val (decoded, nbrOfBlocksSolved) =
-  //    codec.decode(encoded.toSeq, inputData.length)
+    val inputData = dataToUnencodedPackages(
+      Seq.fill(5)(Seq.fill(5)(0))
+    )
+    val codec = new FountainsCodes()
+    val encoded = codec.encode(inputData)
+    val (decoded, nbrOfBlocksSolved) =
+      codec.decode(encoded.toSeq, inputData.length)
 
-  //  nbrOfBlocksSolved should be(inputData.length)
-  //  decoded should be(inputData)
-  //}
+    nbrOfBlocksSolved should be(inputData.length)
+    decoded.map(_.data.toSeq) should be(inputData.map(_.data.toSeq))
+  }
 }
